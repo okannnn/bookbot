3c3f3539722619dfbd1c3b1774a18572f8a424e7
okannnn <okanque@gmail.com>
2024-11-07 17:05:13 +0000

    added report function to main()

--- main.py
+++ main.py
@@ -29,17 +29,57 @@ def characterProcess(text):
         else:
             characterDict.update({j:1})
             #print("Value Added")
-    #print(characterDict)
-    
+    return(characterDict)
+   
+def report(filepath, word_count_str, characterDict):
+    title_String = f"--- Begin Report of {filepath}"
+    print(title_String)
+    print(word_count_str)
 
+    dictKeys = []
+    dictValues = []
+    
+    for i in characterDict:
+        dictKeys.append(i)
+        #print(i)
+        dictValues.append(characterDict[i])
+        #print(characterDict[i])
+    
+    dictValues.sort()
+    sortedDictKeys = []
+    for i in range(len(dictValues) -1, 0, -1):
+        #print(dictValues)
+
+        mydict = {'george': 16, 'amber': 19}
+        sortedDictKeys.append(list(characterDict.keys())[list(characterDict.values()).index(dictValues[i])])
+        #print(list(characterDict.keys())[list(characterDict.values()).index(dictValues[i])])
+    dictValues.reverse()
+    for i in range(len(dictValues)-1):
+        print(f"The {sortedDictKeys[i]} character was found {dictValues[i]} times")
+
+    print("End Report")
+
+def strippedCharacterDict(character_dict):
+    list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', "!", "@", "£", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", ";", ":", "'", '"', "\\" "|", "<", ",", ".", "/", "?", "\n", " "]
+
+    for i in range(0, len(list)):
+        if list[i] in character_dict:
+            character_dict.pop(list[i])
+    
+    return character_dict
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     book = import_file_contents(book_path)
     #print(book)
     word_count = wordCount(book)
-    print(f"there are {word_count} words in this document")
+    word_count_str = f"there are {word_count} words in this document"
 
-    characterProcess(book)
+    characterDict = characterProcess(book)
+    #print(characterDict)
+
+    stripped_character_dict = strippedCharacterDict(characterDict)
+
+    report(book_path, word_count_str, stripped_character_dict)
 
 main()
