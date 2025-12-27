8014d35c22ef07cd3d24d227f306f79557a8966d
okannnn <okanque@gmail.com>
2025-12-27 10:41:22 +0000

    Added functionality to pass in path of book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-from stats import count_words
-
+from stats import count_words, return_characters
+import sys
 
 def get_book_text(filepath):
     
@@ -9,17 +9,47 @@ def get_book_text(filepath):
     
     return book
 
+def generate_report(filepath, noWords, SL):
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {noWords} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(SL)):
+        print(f"{SL[i][1]}: {SL[i][0]}")
 
+def dictionary_sort(D):
+    L = []
+    for i in D:
+        L.append((D[i], i))
 
+    SL = sorted(L, reverse=True)
 
-
+    return SL
+  
 
 def main():
+    
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+    #filepath = 'books/frankenstein.txt'
 
-    bookString = get_book_text("./books/frankenstein.txt")
+    bookString = get_book_text(filepath)
     #print(bookString)
     noWords = count_words(bookString)
-    print(f"Found {noWords} total words")
+    
 
-main()
+    D = return_characters(bookString)
+    
+
+    SL = dictionary_sort(D)
+    generate_report(filepath, noWords, SL)
+
+try:
+    main()
+except Exception as e:
+    print(e)
--- stats.py
+++ stats.py
@@ -5,3 +5,14 @@ def count_words(string):
     noWords = len(array)
     return noWords
 
+def return_characters(string):
+    lowerString = string.lower()
+    Dict = {}
+
+    for i in range(0, len(string)):
+        if lowerString[i] in Dict:
+            Dict[lowerString[i]] += 1
+        else:
+            Dict[lowerString[i]] = 1
+        
+    return Dict
